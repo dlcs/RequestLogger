67e011414da75c5d6a67107f5016c1e05565089f
jack.lewis <jack.lewis@digirati.com>
2023-07-07 17:00:58 +0100

    adding in request blacklists

--- src/RequestLogger/Settings/BlacklistSettings.cs
+++ src/RequestLogger/Settings/BlacklistSettings.cs
@@ -2,11 +2,23 @@
 
 public class BlacklistSettings
 {
+    /// <summary>
+    /// Header blacklist
+    /// </summary>
     public List<string> HeaderBlacklist { get; init; } = new();
 
+    /// <summary>
+    /// Query parameter blacklist
+    /// </summary>
     public List<string> QueryParamBlacklist { get; init; } = new();
 
+    /// <summary>
+    /// Endpoint blacklist
+    /// </summary>
     public List<string> EndpointBlacklist { get; init; } = new();
 
+    /// <summary>
+    /// Request body blacklist
+    /// </summary>
     public List<string> BodyStorageBlacklist { get; set; } = new();
 }
--- src/RequestLogger/Settings/RequestLoggerSettings.cs
+++ src/RequestLogger/Settings/RequestLoggerSettings.cs
@@ -2,5 +2,8 @@
 
 public class RequestLoggerSettings
 {
+    /// <summary>
+    /// Settings related to configuring blacklists
+    /// </summary>
     public BlacklistSettings BlacklistSettings { get; set; } = new();
 }
