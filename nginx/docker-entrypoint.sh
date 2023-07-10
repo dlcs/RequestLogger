fcf6cb01d9af93bd7781e32c5b26398c5823ab01
jack.lewis <jack.lewis@digirati.com>
2023-07-10 15:51:16 +0100

    allowing the resolver to be set from environment variables

--- nginx/docker-entrypoint.sh
+++ nginx/docker-entrypoint.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 set -eu
 
-envsubst '${EXTERNAL_PORT} ${NGINX_PROXY_PASS_LOCATION} ${NGINX_PROXY_PASS_MIRROR_LOCATION} ${SERVICE}' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
+envsubst '${EXTERNAL_PORT} ${NGINX_PROXY_PASS_LOCATION} ${NGINX_PROXY_PASS_MIRROR_LOCATION} ${SERVICE} ${NGINX_RESOLVER}' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
 
 exec "$@"
